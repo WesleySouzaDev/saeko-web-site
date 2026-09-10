bbf11f61a0c8105cc2e0bd71dda78b6c3ed49497
WesleySouzaDev <wesleyawm311@gmail.com>
2026-09-09 21:49:46 -0300

    feat: add theming logos

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -11,19 +11,14 @@ import {
 
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { ToggleLogoTheming } from "./helpers/toggle-logo=theming";
 
 export function AppSidebar() {
 	return (
 		<Sidebar>
 			<SidebarHeader className="border-b border-foreground/10">
 				<div className="h-16 overflow-hidden flex items-center justify-center pb-2">
-					<Image
-						src="/logo.png"
-						alt="Logo"
-						width={200}
-						height={100}
-						className="select-none "
-					/>
+					<ToggleLogoTheming />
 				</div>
 			</SidebarHeader>
 			<SidebarMenu>
---
+++ components/helpers/toggle-logo=theming.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+export function ToggleLogoTheming() {
+	const { theme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (theme === "dark") {
+		return (
+			<Image
+				src={"/logo.png"}
+				alt="Logo"
+				width={200}
+				height={100}
+				className="select-none "
+			/>
+		);
+	} else {
+		return (
+			<Image
+				src={"/logo-light.png"}
+				alt="Logo"
+				width={200}
+				height={100}
+				className="select-none "
+			/>
+		);
+	}
+}
